46f1a60de78e3bab6a17ff3fce11578a4ef6830a
sbalint98 <sbsbalint14@gmail.com>
2019-11-16 21:45:37 +0100

    Updated cuda install script to cuda 10.1

--- install/scripts/install-cuda.sh
+++ install/scripts/install-cuda.sh
@@ -1,10 +1,10 @@
 export INSTALL_PREFIX=${INSTALL_PREFIX:-/opt/hipSYCL}
 
-CUDA_INSTALLER_FILENAME=cuda_10.0.130_410.48_linux
+CUDA_INSTALLER_FILENAME=cuda_10.1.243_418.87.00_linux.run
 
 set -e
 cd /tmp
 if [ ! -f $CUDA_INSTALLER_FILENAME ]; then
-  wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/$CUDA_INSTALLER_FILENAME
+  wget https://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/$CUDA_INSTALLER_FILENAME
 fi
-sh $CUDA_INSTALLER_FILENAME --override --silent --toolkit --toolkitpath $INSTALL_PREFIX/cuda
+sh $CUDA_INSTALLER_FILENAME --override --silent --toolkit --toolkitpath=$INSTALL_PREFIX/cuda
