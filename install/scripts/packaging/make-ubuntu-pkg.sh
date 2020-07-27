3822bd57f63a102f70d60db5b738ae78057341bf
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2020-07-27 21:23:43 +0200

    Add boost.context, boost.fiber as requirement for the packages

--- install/scripts/packaging/make-ubuntu-pkg.sh
+++ install/scripts/packaging/make-ubuntu-pkg.sh
@@ -21,7 +21,7 @@ Version: ${HIPSYCL_VERSION_STRING}
 Section: base
 Priority: optional
 Architecture: amd64
-Depends: hipsycl-base${HIPSYCL_PKG_NAME_SUFFIX}  (>= 0.8), python3 (>= 3.0)
+Depends: hipsycl-base${HIPSYCL_PKG_NAME_SUFFIX}  (>= 0.8), python3 (>= 3.0), libboost-fiber1.65-dev, libboost-context1.65-dev, libboost-filesystem1.65-dev
 Maintainer: Aksel Alpay <aksel.alpay@uni-heidelberg.de>
 Description: hipSYCL${HIPSYCL_VERSION_STRING}
  Implementation of Khronos SYCL for CPUs, AMD GPUs and NVIDIA GPUs 
