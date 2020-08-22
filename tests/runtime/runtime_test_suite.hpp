749c0e4d58209b021f569a7acb6b078d235f0579
sbalint98 <sbsbalint14@gmail.com>
2020-08-22 16:10:46 +0200

    finished adding tests for the data_region class

--- tests/runtime/runtime_test_suite.hpp
+++ tests/runtime/runtime_test_suite.hpp
@@ -26,15 +26,56 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <hipSYCL/runtime/serialization/serialization.hpp>
 #include <hipSYCL/runtime/application.hpp>
+#include <hipSYCL/runtime/util.hpp>
+#include <hipSYCL/runtime/data.hpp>
+
 #include <boost/test/unit_test.hpp>
 
 #ifndef HIPSYCL_RT_TESTS_HPP
 #define HIPSYCL_RT_TESTS_HPP
 
+using namespace hipsycl;
+
+struct mark_current_configuration{
+  std::vector<rt::device_id> device_list;
+  std::vector<std::pair<rt::device_id, std::vector<rt::range_store::rect> > > make_current_list;
+  std::vector<std::pair<rt::device_id, std::vector<rt::range_store::rect> > > expected_outdated;
+
+  friend std::ostream &operator<<(std::ostream &out, const mark_current_configuration &v);
+};
+
+std::ostream &operator<<(std::ostream &out, const mark_current_configuration &v);
+
+struct mark_valid_configuration {
+  std::vector<rt::device_id> device_list;
+  std::vector<std::pair<rt::device_id, std::vector<rt::range_store::rect> > > make_current_list;
+  std::vector<std::pair<rt::device_id, std::vector<rt::range_store::rect> > > make_valid_list;
+  std::vector<std::pair<rt::device_id, std::vector<rt::range_store::rect> > > expected;
+  friend std::ostream &operator<<(std::ostream &out, const mark_valid_configuration &v);
+};
+
+std::ostream &operator<<(std::ostream &out, const mark_valid_configuration &v);
+
+struct update_source{
+  rt::device_id target_device;
+  std::vector<std::pair<rt::device_id, rt::range_store::rect> >  source_region_per_device;
+};
+
+struct update_source_candidates_configuration{
+  std::vector<rt::device_id> device_list;
+  std::vector<std::pair<rt::device_id, std::vector<rt::range_store::rect> > > make_current_list;
+  std::vector<std::pair<rt::device_id, std::vector<rt::range_store::rect> > > make_valid_list;
+  std::vector<update_source> expected;
+  friend std::ostream &operator<<(std::ostream &out, const mark_valid_configuration &v);
+};
+
+std::ostream &operator<<(std::ostream &out, const update_source_candidates_configuration &v);
+
 struct shutdown_runtime {
   ~shutdown_runtime() {
-    hipsycl::rt::application::reset();
+   hipsycl::rt::application::reset();
   }
 };
 
