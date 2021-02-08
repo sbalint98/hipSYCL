1f307405257b3e2aa15e902ec97c4a5a3166793c
sbalint98 <sbsbalint14@gmail.com>
2021-02-08 20:45:59 +0100

    Reduce size of the base package

--- install/scripts/spack-install-llvm.sh
+++ install/scripts/spack-install-llvm.sh
@@ -15,8 +15,11 @@ export PATH=$SPACK_ROOT/bin:$PATH
 sed -i 's|root: .*$|root: /opt/hipSYCL/llvm/|' spack/etc/spack/defaults/config.yaml
 sed -i 's|all: .*$|all: ${PACKAGE}|' spack/etc/spack/defaults/config.yaml
 sed -i 's|# build_jobs: .*$|build_jobs: 64|' spack/etc/spack/defaults/config.yaml
+sed -i 's|projects.append("clang-tools-extra")|#projects.append("clang-tools-extra")|' spack/var/spack/repos/builtin/packages/llvm/package.py
+
 . $SPACK_ROOT/share/spack/setup-env.sh
-spack install llvm@$llvm_version cuda=False libcxx=False target=x86_64
+spack install llvm@11.0.0 cuda=False libcxx=False polly=False lldb=False lld=False internal_unwind=False gold=False target=x86_64 build_type=MinSizeRel -flang 
+#spack install llvm@$llvm_version cuda=False libcxx=False target=x86_64
 spack load llvm
 spack compiler find /opt/hipSYCL/llvm/llvm/
 spack unload llvm
