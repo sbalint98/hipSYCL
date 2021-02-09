c8b7f647fbc7f419c62fe7f287d9a709b9ced9cc
sbalint98 <sbsbalint14@gmail.com>
2021-02-09 15:00:02 +0100

    We need lld anyways for compiling for hip :P

--- install/scripts/spack-install-llvm.sh
+++ install/scripts/spack-install-llvm.sh
@@ -18,7 +18,7 @@ sed -i 's|# build_jobs: .*$|build_jobs: 64|' spack/etc/spack/defaults/config.yam
 sed -i 's|projects.append("clang-tools-extra")|#projects.append("clang-tools-extra")|' spack/var/spack/repos/builtin/packages/llvm/package.py
 
 . $SPACK_ROOT/share/spack/setup-env.sh
-spack install llvm@11.0.0 cuda=False libcxx=False polly=False lldb=False lld=False internal_unwind=False gold=False target=x86_64 build_type=MinSizeRel -flang 
+spack install llvm@11.0.0 cuda=False libcxx=False polly=False lldb=False lld=True internal_unwind=False gold=False target=x86_64 build_type=MinSizeRel -flang 
 #spack install llvm@$llvm_version cuda=False libcxx=False target=x86_64
 spack load llvm
 spack compiler find /opt/hipSYCL/llvm/llvm/
